e236d8523329000631fe8ac439e612c7f99b30f4
Sindhuja Peravali <Sindhuja.p93@gmail.com>
2022-12-16 16:03:47 +0530

    Added additional checks

--- src/test/java/LearningGit1.java
+++ src/test/java/LearningGit1.java
@@ -4,6 +4,7 @@ public class LearningGit1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Test");
+		System.out.println("Added check");
 	}
 
 }
